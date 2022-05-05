abb844e3578ff8a1678fef948e316230c5e5c909
leofravega51 <leo-fravega@hotmail.com>
2022-05-05 11:31:51 -0300

    Adding and refactoring functions

--- functions.py
+++ functions.py
@@ -14,6 +14,7 @@ def insertInDataTable(self):
 
     for item in self.data:
         rowPosition = self.dataTable.rowCount()
+        columnCount = self.dataTable.columnCount()
         self.dataTable.insertRow(rowPosition)
 
         self.dataTable.setItem(
@@ -34,6 +35,17 @@ def insertInDataTable(self):
             rowPosition, 7, QtWidgets.QTableWidgetItem(str(item['variedad'])))
         self.dataTable.setItem(
             rowPosition, 8, QtWidgets.QTableWidgetItem(str(item['batch'])))
+        
+        if(float(item["temperatura"]) < 30):
+            setColorToRow(self.dataTable, rowPosition, columnCount, QtGui.QColor(135,206,235))
+        elif(float(item["temperatura"]) >= 30 and float(item["temperatura"]) < 60):
+            setColorToRow(self.dataTable, rowPosition, columnCount, QtGui.QColor(180,255,159))
+        else:
+            setColorToRow(self.dataTable, rowPosition, columnCount, QtGui.QColor(218,18,18))
+        
+def setColorToRow(dataTable, rowPosition, columnCount, color):
+    for col in range(columnCount):
+        dataTable.item(rowPosition, col).setBackground(color)
 
 
 def getDate(dateEdit):
@@ -53,8 +65,7 @@ def setBeginDate(self, date):
 
 def setEndDate(self, date):
     self.end_date = date.toPyDate().strftime(f'%Y-%m-%d')
-    if(self.begin_date != None and self.begin_date <= self.end_date):
-        sortByDates(self.dataTable, 1, self.begin_date, self.end_date)
+
     return self.end_date
 
 
@@ -73,13 +84,54 @@ def setChartInput(self, text):
 
 def getFilterText(self, text):
     self.input_data = text
-    if(self.column_filter != None):
-        self.form_data = sortByInput(
-            self.dataTable, self.column_filter, self.input_data)
+
+    return self.input_data
+
+
+def filterTable(self):
+    """Filtramos la tabla segun las opciones cargadas para filtrar (entre fechas, entre fechas y segun una columna, segun una columna)"""
+
+    if(self.begin_date != None and self.end_date != None and self.begin_date <= self.end_date):
+        if(self.column_filter != None and self.input_data != None):
+            sortByDatesAndInput(self)
+        else:
+            sortByDates(self.dataTable, 1, self.begin_date, self.end_date)
+    else:
+        if(self.column_filter != None and self.input_data != None):
+            self.form_data = sortByInput(
+                self.dataTable, self.column_filter, self.input_data)
+
+
+def sortByDatesAndInput(self):
+    
+    for rowIndex in range(self.dataTable.rowCount()):
+        twItem = self.dataTable.item(rowIndex, 1)
+        inputItem = self.dataTable.item(rowIndex, self.column_filter)
+
+        sp1 = twItem.text().split(" ")
+        date = sp1[0].split("-")
+        itemDate = datetime.datetime(int(date[0]), int(date[1]), int(date[2]))
+
+        splitBeginDate = self.begin_date.split("-")
+        splitEndDate = self.end_date.split("-")
+        by, bm, bd = int(splitBeginDate[0]), int(
+            splitBeginDate[1]), int(splitBeginDate[2])
+        ey, em, ed = int(splitEndDate[0]), int(
+            splitEndDate[1]), int(splitEndDate[2])
+        bDate = datetime.datetime(by, bm, bd)
+        eDate = datetime.datetime(ey, em, ed)
+
+
+        if bDate == eDate and bDate == itemDate and str.startswith(str(inputItem.text()), str(self.input_data)):
+            self.dataTable.setRowHidden(rowIndex, False)
+        elif itemDate >= bDate and itemDate <= eDate and str.startswith(str(inputItem.text()), str(self.input_data)):
+            self.dataTable.setRowHidden(rowIndex, False)
+        else:
+            self.dataTable.setRowHidden(rowIndex, True)
 
 
 def sortByDates(dataTable, columnOfInterest, beginDate, endDate):
-    """"""
+    """Filtrado de datos en tabla dado por 2 fechas extremo"""
 
     for rowIndex in range(dataTable.rowCount()):
         twItem = dataTable.item(rowIndex, columnOfInterest)
@@ -125,10 +177,22 @@ def sortByInput(dataTable, columnOfInterest, valueOfInterest):
     return form_data
 
 
+def clearFilterTable(self):
+    """Limpiamos los filtros realizados previamente"""
+
+    for rowIndex in range(self.dataTable.rowCount()):
+        self.dataTable.setRowHidden(rowIndex, False)
+
+def setMinTemp(self, min):
+    self.temp_min = min
+
+def setMaxTemp(self, max):
+    self.temp_max = max
+
 def drawChart(self):
     if(self.chart_input and self.chart_date and self.chart_input != "Secadora"):
         self.chart = Chart(
-            self.chart_input, self.chart_date, self.temperaturas)
+            self.chart_input, self.chart_date, self.temperaturas, self.temp_min, self.temp_max)
         self.chart.resize(1280, 700)
         self.chart.show()
     else:
@@ -187,10 +251,12 @@ def exportToExcel(self):
     self.end_date = None
 
 
-def enableExportButton(self):
+def enableButton(self, button):
     """Habilitamos el boton de Exportar a Excel luego de evaluar que se haya registrado una fecha rango o un filtro por columna"""
 
     if((self.begin_date and self.end_date) or (self.column_filter and self.input_data) or (self.begin_date and self.end_date and self.column_filter and self.input_data)):
-        self.exportToExcelButton.setEnabled(True)
+        button.setEnabled(True)
     else:
-        self.exportToExcelButton.setEnabled(False)
+        button.setEnabled(False)
+
+
